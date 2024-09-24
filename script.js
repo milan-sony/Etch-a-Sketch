fc53532b8ec0e821a34498e010e8fa764ebfca9c
Milan Sony <milansonyofficial@gmail.com>
2024-09-24 17:30:55 +0530

    Colour picker color added as the grid color

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const colorPicker = document.getElementById('colorPicker');
 
 let rows = 16;
 let cols = 16;
-
-
+let colorPickerValue = ''
 
 // created a style property named --grid-rows (css)
 gridContainer.style.setProperty('--grid-rows', rows);
@@ -13,9 +12,18 @@ gridContainer.style.setProperty('--grid-cols', cols);
 for (let c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     gridContainer.appendChild(cell).className = "grid-item";
+    gridContainer.addEventListener('mouseover', changeColor)
 };
 
 
+function changeColor(e){
+    console.log(e)
+    if(e.type == "mouseover"){
+        e.target.style.backgroundColor = colorPickerValue
+    }
+}
+
 function color(){
     console.log("Color:", colorPicker.value)
+    colorPickerValue = colorPicker.value
 }
