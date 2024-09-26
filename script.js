b26794f9f62432a95a94138b884e24cb69c04992
Milan Sony <milansonyofficial@gmail.com>
2024-09-26 09:32:18 +0530

    Semicolon added at the end in JS

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ const clearBtn = document.getElementById('clearBtn');
 
 let rows = 16;
 let cols = 16;
-let colorPickerValue = '#000000'
+let colorPickerValue = '#000000';
 
 colorPicker.oninput = () => color();
 
 function setMenuMode(color) {
-    let menuMode = color
-    console.log(menuMode)
+    let menuMode = color;
+    console.log(menuMode);
     if (menuMode === 'color') {
-        colorBtn.classList.add('active-btn')
-        eraseBtn.classList.remove('active-btn')
+        colorBtn.classList.add('active-btn');
+        eraseBtn.classList.remove('active-btn');
     } else if (menuMode === 'erase') {
-        colorBtn.classList.remove('active-btn')
-        eraseBtn.classList.add('active-btn')
+        colorBtn.classList.remove('active-btn');
+        eraseBtn.classList.add('active-btn');
     } else {
-        colorBtn.classList.add('active-btn')
+        colorBtn.classList.add('active-btn');
     }
 }
 
@@ -33,21 +33,21 @@ gridContainer.style.setProperty('--grid-cols', cols);
 for (let c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     gridContainer.appendChild(cell).className = "grid-item";
-    gridContainer.addEventListener('mouseover', changeColor)
+    gridContainer.addEventListener('mouseover', changeColor);
 };
 
 function changeColor(e) {
     console.log(e)
     if (e.type == "mouseover") {
-        e.target.style.backgroundColor = colorPickerValue
+        e.target.style.backgroundColor = colorPickerValue;
     }
 }
 
 function color() {
-    console.log("Color:", colorPicker.value)
-    colorPickerValue = colorPicker.value
+    console.log("Color:", colorPicker.value);
+    colorPickerValue = colorPicker.value;
 }
 
 window.onload = () => {
-    setMenuMode('color')
+    setMenuMode('color');
 }
