a54ab2318a6b706390f5140e15532490891b8909
Milan Sony <milansonyofficial@gmail.com>
2024-09-27 11:53:55 +0530

    Clear btn function added, Grid size changed as of the range slider value, UI modified

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ const size = document.getElementById('size');
 
 let defaultColor = '#000000';
 let defaultMode = 'color';
-
-let rows = 16;
-let cols = 16;
+let defaultSize = slider.value;
 
 let currentColor = defaultColor;
 let currentMode = defaultMode
+let currentSize = defaultSize;
 
 colorBtn.onclick = () => setMenuMode('color');
 eraseBtn.onclick = () => setMenuMode('erase');
@@ -22,9 +21,11 @@ clearBtn.onclick = () => setMenuMode('clear');
 colorPicker.oninput = () => pickColor();
 slider.oninput = () => sliderMove();
 
-function sliderMove(){
+function sliderMove() {
     console.log(slider.value)
-    size.innerText = slider.value;
+    size.innerText = slider.value + " X " + slider.value;
+    currentSize = parseInt(slider.value);
+    createGrid(currentSize)
 }
 
 function setMenuMode(mode) {
@@ -40,24 +41,29 @@ function setMenuMode(mode) {
         currentColor = 'transparent';
     } else if (currentMode === 'clear') {
         clearGrid();
-        // gridContainer.style.backgroundColor = 'transparent'
     }
     else {
         colorBtn.classList.add('active-btn');
     }
 };
 
-// created a style property named --grid-rows (css)
-gridContainer.style.setProperty('--grid-rows', rows);
-gridContainer.style.setProperty('--grid-cols', cols);
-for (let c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    gridContainer.appendChild(cell).className = 'grid-item';
-    gridContainer.addEventListener('mouseover', paintColor);
-};
+function createGrid(size) {
+    gridContainer.innerHTML = "";
+    // created a style property named --grid-rows (css)
+    gridContainer.style.setProperty('--grid-rows', size);
+    gridContainer.style.setProperty('--grid-cols', size);
+    for (let c = 0; c < (size * size); c++) {
+        let cell = document.createElement("div");
+        gridContainer.appendChild(cell).className = 'grid-item';
+        gridContainer.addEventListener('mouseover', paintColor);
+    };
 
-function clearGrid(){
-    gridContainer.style.backgroundColor = 'green'
+}
+
+
+function clearGrid() {
+    createGrid(currentSize)
+    setMenuMode('color')
 }
 
 function paintColor(e) {
@@ -76,6 +82,6 @@ function pickColor() {
 window.onload = () => {
     setMenuMode('color');
     currentColor = defaultColor
-    size.innerText = slider.value;
-
+    size.innerText = slider.value + " X " + slider.value;
+    createGrid(defaultSize)
 }
